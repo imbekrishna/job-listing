bc2fd00f6a5ee3fc735b7998d9ec16d8e90f84f1
imbekrishna <136263294+imbekrishna@users.noreply.github.com>
2024-03-12 14:52:51 +0530

    added user model

---
+++ models/user.js
@@ -0,0 +1,25 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    minLength: 3,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  mobile: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+});
+
+const User = mongoose.model("User", userSchema);
+
+module.exports = User;
